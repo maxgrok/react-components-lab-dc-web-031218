0bde45d02a51979c05f5581ad9e37d6d94958955
Max Goodman <max.goodman@flatironschool.com>
2018-06-12 19:27:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
@@ -7,21 +11,56 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends React.Component {
-  // your code here
+  render(){
+    return (
+      <div className="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+      )
+  }
 }
 
 export class InFrontOfYou extends React.Component {
-  // your code here
+  render(){
+    return (
+        <div>
+          <p>You shouldn't look too far.</p>
+          <p>Sometimes, the solution is right in front of you.</p>
+        </div>
+      )
+  }
 }
+const products = [
+  <li>{BUTCHER_PRODUCTS[0]}</li>,
+  <li>{BUTCHER_PRODUCTS[1]}</li>,
+  <li>{BUTCHER_PRODUCTS[2]}</li>,
+  <li>{BUTCHER_PRODUCTS[3]}</li>
+]
 
 export class ButcherShop extends React.Component {
-  // your code here
+  render(){
+    return (
+      <div className="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          {products}
+        </ul>
+      </div>
+      )
+  }
 }
 
 
 ReactDOM.render(
-  <div>
-    render your components here! Refer back to the previous README if you are lost
-  </div>,
-  document.getElementById('root')
+    <div>
+      <OlderCoaster />
+      <InFrontOfYou />
+      <ButcherShop />
+    </div>,
+    document.getElementById('root')
 )
